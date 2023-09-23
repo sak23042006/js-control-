3bbbc091b2f8da6ba4c2f447ef0c6f46e105cc14
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-09-23 23:59:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-control-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-4gn4zd)
---
+++ script.js
@@ -0,0 +1,13 @@
+// Challenge 1: Create a for loop to print all the even numbers till 100 in the console.
+for (i = 0, i; i < 101; i += 2) {
+  console.log(i);
+}
+// Challenge 2: Create a while loop to print all the odd numbers till 100 in the console.
+var i = 1;
+while (i < 101) {
+  console.log(i);
+  i += 2;
+}
+// Challenge 3: Create a do while loop to print all the multiples of 3 till 100 in the console.
+
+// Challenge 4: Create a for loop to print all the prime numbers till 100 in the console.
